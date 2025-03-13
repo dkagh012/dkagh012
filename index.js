35507cec638f7516ebc3ba46687e05b49e8c4b8e
dkagh012 <5064664@naver.com>
2025-03-13 11:31:56 +0900

    index.js 수정정

--- index.js
+++ index.js
@@ -35,22 +35,51 @@ const parser = new Parser({
 
 # 👩🏻‍💻 Skills 
 
-- Front-End  
-  <img src="https://img.shields.io/badge/html5-black?style=for-the-badge&logo=html5&logoColor=#E34F26"> 
-  <img src="https://img.shields.io/badge/javascript-black?style=for-the-badge&logo=javascript&logoColor=#F7DF1E"> 
-  <img src="https://img.shields.io/badge/typescript-black?style=for-the-badge&logo=typescript&logoColor=#3178C6"> 
-  <img src="https://img.shields.io/badge/React-black?style=for-the-badge&logo=react&logoColor=#61DAFB"> 
-  <img src="https://img.shields.io/badge/recoil-black?style=for-the-badge&logo=recoil&logoColor=#3578E5"> 
+<div align="justify">
+  
+<br/>
 
-- Tools  
-  <img src="https://img.shields.io/badge/figma-black?style=for-the-badge&logo=figma&logoColor=#F24E1E"> 
-  <img src="https://img.shields.io/badge/git-black?style=for-the-badge&logo=git&logoColor=#F05032"> 
+- Front-End
 
-- Collaboration  
-  <img src="https://img.shields.io/badge/notion-black?style=for-the-badge&logo=notion&logoColor=#000000"> 
-  <img src="https://img.shields.io/badge/discord-black?style=for-the-badge&logo=discord&logoColor=#5865F2"> 
+<img src="https://img.shields.io/badge/html5-black?style=for-the-badge&logo=html5&logoColor=#E34F26"> 
+<img src="https://img.shields.io/badge/javascript-black?style=for-the-badge&logo=javascript&logoColor=#F7DF1E"> 
+<img src="https://img.shields.io/badge/typescript-black?style=for-the-badge&logo=typescript&logoColor=#3178C6"> 
+<img src="https://img.shields.io/badge/React-black?style=for-the-badge&logo=react&logoColor=#61DAFB"> 
+<img src="https://img.shields.io/badge/recoil-black?style=for-the-badge&logo=recoil&logoColor=#3578E5"> 
+<img src="https://img.shields.io/badge/reactquery-black?style=for-the-badge&logo=reactquery&logoColor=#FF4154"> 
+<img src="https://img.shields.io/badge/reacthookform-black?style=for-the-badge&logo=reacthookform&logoColor=#EC5990"> 
+<img src="https://img.shields.io/badge/-styled--components-black?style=for-the-badge&logo=styledcomponents&logoColor=#DB7093"> 
+<img src="https://img.shields.io/badge/css3-black?style=for-the-badge&logo=css3&logoColor=1572B6"> 
+<img src="https://img.shields.io/badge/Sass-black?style=flat-square&amp;logo=Sass&amp;logoColor=white">
 
-# 📕 Latest Blog Posts
+<br/>
+
+
+- Tools
+
+<img src="https://img.shields.io/badge/figma-black?style=for-the-badge&logo=figma&logoColor=#F24E1E"> 
+<img src="https://img.shields.io/badge/git-black?style=for-the-badge&logo=git&logoColor=#F05032"> 
+<img src="https://img.shields.io/badge/github-black?style=for-the-badge&logo=github&logoColor=#181717"> 
+<img src="https://img.shields.io/badge/postman-black?style=for-the-badge&logo=postman&logoColor=#FF6C37"> 
+
+
+<br/>
+
+- Collaboration
+
+<img src="https://img.shields.io/badge/notion-black?style=for-the-badge&logo=notion&logoColor=#000000"> <img src="https://img.shields.io/badge/discord-black?style=for-the-badge&logo=discord&logoColor=#5865F2"> <img src="https://img.shields.io/badge/slack-black?style=for-the-badge&logo=slack&logoColor=#4A154B"> 
+
+</div>
+
+<br/>
+
+
+<a href="https://github.com/ashutosh00710/github-readme-activity-graph">
+<img src="https://github-readme-activity-graph.vercel.app/graph?username=dkagh012&theme=react-dark&bg_color=20232a&hide_border=true&line=8A87D0&color=a888f7" width=98%/>
+</a>
+
+
+# 📕 최근 블로그 글
 ${latestPosts}
 `;
 
